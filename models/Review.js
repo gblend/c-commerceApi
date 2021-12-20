8f8538f4f1da5b23f52e70f55e1738b40fe042eb
Gabriel Ilochi <gabrielilo190@gmail.com>
2021-12-20 09:06:52 +0100

    Add review schema

---
+++ models/Review.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const joi = require('joi');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+    rating: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        trim: true,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    product: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Product',
+        required: true
+    },
+}, { timestamps: true });
+
+ReviewSchema.index({product: 1, user: 1}, { unique: true });
+const Review = mongoose.model('Review', ReviewSchema);
+
+const validateReviewSchema = (reviewSchema) => {
+    const review = joi.object({
+        rating: joi.number().min(1).max(5).required(),
+        title: joi.string().required(),
+        comment: joi.string().required(),
+        user: joi.object().required(),
+        product: joi.object().required()
+    });
+    return review.validate(reviewSchema);
+}
+
+module.exports = {
+    Review,
+    validateReviewSchema
+}
