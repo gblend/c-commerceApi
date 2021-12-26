6b02328a702445e3a0b167cff0aa0295b2c2f40a
Gabriel Ilochi <gabrielilo190@gmail.com>
2021-12-26 18:02:06 +0100

    Check connection string

--- app.js
+++ app.js
@@ -67,6 +67,7 @@ app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 3000;
 const start = async () => {
+    console.log('db url', process.env.MONGO_URI);
     await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
         console.log(`server listening on port ${port}`);
