0cc91a9a648b708c014ffe68245c7b285a8ee37a
Gabriel Ilochi <gabrielilo190@gmail.com>
2021-12-29 04:57:28 +0100

    Refactor access and refresh token implementation

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,16 +1,24 @@
 const jwt = require('jsonwebtoken');
 
 const createJWT = (payload) => {
-    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRATION});
+    return jwt.sign(payload, process.env.JWT_SECRET);
 }
 
 const isTokenValid = (token) => jwt.verify(token, process.env.JWT_SECRET);
 
-const attachCookiesToResponse = (token, res) => {
+const attachCookiesToResponse = ({accessTokenJWT, refreshTokenJWT, res}) => {
     const oneDay = 60 * 60 * 24 * 1000;
-    res.cookie('token', token,
+    const thirtyDays = 60 * 60 * 24 * 1000 * 30;
+    res.cookie('accessToken', accessTokenJWT,
         {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            signed: true,
             expires: new Date(Date.now() + oneDay),
+        });
+    res.cookie('refreshToken', refreshTokenJWT,
+        {
+            expires: new Date(Date.now() + thirtyDays),
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
             signed: true
