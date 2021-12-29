699da5da8eea491ad4c5dccafcf1090f1a548474
Gabriel Ilochi <gabrielilo190@gmail.com>
2021-12-29 04:29:00 +0100

    Refactor access token flow and add refresh token

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,20 +1,34 @@
 const CustomErr =  require('../errors');
-const { isTokenValid } = require('../utils');
+const { isTokenValid, attachCookiesToResponse} = require('../utils');
+const {Token} = require("../models/Token");
+const {createJWT} = require("../utils/jwt");
 
 
 const authenticateUser = async (req, res, next) => {
-    const token = req.signedCookies.token;
-
-    if (!token) {
-        throw new CustomErr.UnauthenticatedError('error, no token present');
-    }
+    const {accessToken, refreshToken} = req.signedCookies;
 
     try {
-        const {name, id, role} = isTokenValid(token);
-        req.user = {name, id, role};
+        if(accessToken) {
+            const {name, id, role} = isTokenValid(accessToken);
+            req.user = {name, id, role};
+           return next();
+        }
+
+        const payload = isTokenValid(refreshToken);
+        const isTokenExist = await Token.findOne({
+            user: payload.user.id,
+            refreshToken: payload.refreshToken
+        });
+        if(!isTokenExist || !isTokenExist?.isValid) {
+            throw new CustomErr.UnauthenticatedError('Authentication invalid');
+        }
+
+        const accessTokenJWT = createJWT(payload.user);
+        attachCookiesToResponse({accessTokenJWT, refreshTokenJWT:isTokenExist.refreshToken, res});
+        req.user = payload.user;
         next();
     } catch (err) {
-        throw new CustomErr.UnauthenticatedError('error, authentication failed');
+        throw new CustomErr.UnauthenticatedError('Authentication invalid');
     }
 }
 
