8e7c6443938b5f2fc2e3e43811450d06dd654a74
Gabriel Ilochi <gabrielilo190@gmail.com>
2021-12-29 04:33:48 +0100

    Add verify email, reset and forgot password routes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
 const router = express.Router();
-const { register, login, logout } = require('../controllers/auth-controller');
+const { register, login, logout, verifyEmail, resetPassword, forgotPassword} = require('../controllers/auth-controller');
+const {authenticateUser} = require("../middleware/authentication");
 
 router.post('/register', register);
 router.post('/login', login);
-router.get('/logout', logout);
+router.delete('/logout', authenticateUser, logout);
+router.post('/verify-email', verifyEmail);
+router.post('/reset-password', resetPassword);
+router.post('/forgot-password', forgotPassword);
 
 module.exports = router;
 
