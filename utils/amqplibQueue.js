e74540c3e6c573dcbb4a1896d0477ebbf7ad4400
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-01-13 23:50:38 +0100

    Setup named exchange and bind it to queue

--- utils/amqplibQueue.js
+++ utils/amqplibQueue.js
@@ -17,13 +17,19 @@ const initAmqpServer = async () => {
 const createAmqpChannel = async (queue) => {
     connection = await initAmqpServer();
     channel = await connection.createChannel();
+    channel.assertExchange(queue, 'direct', {durable: true});
     await channel.assertQueue(queue);
     return {channel}
 }
 
 const pushToQueue = async (queue, queueErrorMsg, data) => {
     const {channel:amqpChannel} = await createAmqpChannel(queue);
+    // The empty string as second parameter means that we don't want to send the message to any specific queue (routingKey).
+    // We want only to publish it to our exchange
+    const pubData = amqpChannel.publish(queue, '', Buffer.from(data));
+    console.log('Published data', pubData);
     const queueData = await amqpChannel.sendToQueue(queue, Buffer.from(JSON.stringify({ [queue]: data })));
+    console.log('queueData', queueData);
     if (!queueData) {
         throw new CustomError.BadRequestError(queueErrorMsg);
     }
@@ -32,7 +38,11 @@ const pushToQueue = async (queue, queueErrorMsg, data) => {
 
 const initConsumeQueue = async (fn, queue) => {
     const {channel:ch} = await createAmqpChannel(queue);
+    ch.assertExchange(queue, 'direct', {durable: true});
+    // The third parameter empty string means any type - fanout, direct, topic, header
+    ch.bindQueue(queue, queue, '');
     ch.consume(queue, async (data) => {
+        console.log('routingKey', data.fields.routingKey);
         let queuePayload = JSON.parse(data.content);
         await fn(queuePayload[queue]);
         ch.ack(data)
