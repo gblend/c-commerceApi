14e431e0aa0d9e0e25514ceb96d213585d86fb73
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-01-16 23:58:18 +0100

    Add webhookHandler

--- utils/stripeapi.js
+++ utils/stripeapi.js
@@ -32,6 +32,24 @@ class StripeApi {
 
         return {clientSecret: intent.client_secret, amount: this.amount}
     }
+
+    webhookHandler = (req, res) => {
+        const event = req.body;
+        // Handle the event
+        switch (event.type) {
+            case 'payment_intent.succeeded':
+                const paymentIntent = event.data.object;
+                console.log('PaymentIntent was successful!', paymentIntent);
+                break;
+            case 'payment_method.attached':
+                const paymentMethod = event.data.object;
+                console.log('PaymentMethod was attached to a Customer!', paymentMethod);
+                break;
+            // ... handle other event types
+            default:
+                console.log(`Unhandled event type ${event.type}`);
+        }
+    }
 }
 
 
