851a3b093ec32f3481d1962082b3bb32c5ee023c
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-01-17 14:16:18 +0100

    Add webhook route

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -9,9 +9,11 @@ const {
     updateOrder
 } = require('../controllers/orderController');
 const {authenticateUser, authorizePermissions} = require("../middleware/authentication");
+const {stripeWebhookHandler} = require("../controllers/stripeController");
 
 router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllOrders).post(authenticateUser, createOrder);
 router.route('/userOrders').get(authenticateUser, getCurrentUserOrders);
 router.route('/:id').get(authenticateUser, getSingleOrder).patch(authenticateUser, updateOrder);
+router.route('/webhook').post(stripeWebhookHandler);
 
 module.exports = router;
